c6c4afe6c6c5b80f4ab0a76ac0190883f9df075c
ok <125519004+oConte@users.noreply.github.com>
2023-03-08 21:06:54 -0300

    Add files via upload

---
+++ 8_elseif.php
@@ -0,0 +1,11 @@
+<?php
+	$nome = ".";
+		if($nome == "Igor e Thiago"){
+			echo "O $nome estão estudando";
+		} elseif ($nome == "Jão") {
+			echo "O $nome está estudando";
+		} else {
+			echo "Tem ninguem estudnando nesse trem sô!";
+		}
+
+?>
